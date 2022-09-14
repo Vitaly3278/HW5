7ff48e4718da1ef4c1d4850ab9cad6c9592cfcbb
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-14 15:00:32 +0300

    Even numbers in the array

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int Even (int len)
+{
+    int [] array = new int [len];
+    int count = 0;
+    Console.Write("Ваш массив: ");
+    for (int i = 0; i < len; i++)
+    {
+        array [i] = new Random().Next(100,1000);
+        Console.Write($"{array[i]}, ");
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+   return count;
+}
+Console.Write("Введите размер массива: ");
+int len = int.Parse(Console.ReadLine());
+
+int c = Even(len);
+Console.WriteLine();
+Console.WriteLine($"Четных чисел в массиве: {c}");
