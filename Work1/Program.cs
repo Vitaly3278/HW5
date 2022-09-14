7f566bdf80027e63ac6658af442a812990f47f19
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-14 15:03:02 +0300

    Even numbers in the array

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -1,21 +1,21 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
-int Even (int len)
+int Even(int len)
 {
-    int [] array = new int [len];
+    int[] array = new int[len];
     int count = 0;
     Console.Write("Ваш массив: ");
     for (int i = 0; i < len; i++)
     {
-        array [i] = new Random().Next(100,1000);
+        array[i] = new Random().Next(100, 1000);
         Console.Write($"{array[i]}, ");
         if (array[i] % 2 == 0)
         {
             count++;
         }
     }
-   return count;
+    return count;
 }
 Console.Write("Введите размер массива: ");
 int len = int.Parse(Console.ReadLine());
