150796d6b84e69ff823be68c07bd6c46053d4b15
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-14 15:59:39 +0300

    Difference min max array

---
+++ Work4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+double DoubleArray (int len)
+{
+    double [] array = new double [len];
+    double max = 0;
+    double min = 1;
+    Console.WriteLine("Ваш массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().NextDouble();
+        Console.WriteLine($"{array[i]} ");
+        if (array[i] > max)
+        {
+            max = array[i];
+        } 
+        if (array[i] < min)
+        {
+            min = array[i];
+        } 
+    }
+    double diff = max - min;
+    return diff;
+}
+Console.Write("Введите размер массива: ");
+int len = int.Parse(Console.ReadLine());
+
+double diff = DoubleArray (len);
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна: {diff}");
