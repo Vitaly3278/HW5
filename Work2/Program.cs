518be4c977e34a432c34632dcb829080effb9027
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-14 15:12:16 +0300

    Sum of odd

--- Work2/Program.cs
+++ Work2/Program.cs
@@ -1,20 +1,20 @@
 ﻿//Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
-int SumOdd (int len)
+int SumOdd(int len)
 {
-    int [] array = new int [len];
+    int[] array = new int[len];
     int sum = 0;
     Console.Write("Ваш массив: ");
     for (int i = 0; i < len; i++)
     {
-        array [i] = new Random().Next(1,10);
-        Console.Write($"{array[i]}, ");
+        array[i] = new Random().Next(1, 10);
+        Console.Write($"{array[i]} ");
         if (i % 2 != 0)
         {
             sum = sum + array[i];
         }
     }
-   return sum;
+    return sum;
 }
 Console.Write("Введите размер массива: ");
 int len = int.Parse(Console.ReadLine());
